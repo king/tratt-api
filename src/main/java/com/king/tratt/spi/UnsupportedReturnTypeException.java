d353b1a4c6778d843718253dad7e676e81ac9140
Alexander Poulikakos <alexander.poulikakos@king.com>
2016-04-15 13:37:24 +0200

    Added copyright comment in each file.

--- src/main/java/com/king/tratt/spi/UnsupportedReturnTypeException.java
+++ src/main/java/com/king/tratt/spi/UnsupportedReturnTypeException.java
@@ -1,3 +1,7 @@
+/*******************************************************************************
+ * (C) king.com Ltd 2016
+ *  
+ *******************************************************************************/
 package com.king.tratt.spi;
 
 public class UnsupportedReturnTypeException extends RuntimeException {
