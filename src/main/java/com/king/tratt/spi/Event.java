5df46df9b8eaff1ca7a0ae8dde9719cc78fda438
Alexander Poulikakos <alexander.poulikakos@king.com>
2016-05-09 18:01:58 +0200

    Added javadoc to Event.java

--- src/main/java/com/king/tratt/spi/Event.java
+++ src/main/java/com/king/tratt/spi/Event.java
@@ -5,11 +5,28 @@
 package com.king.tratt.spi;
 
 /**
- * Must be Thread Safe.
+ * Implementations of this should wrap a client event and provide the {@code id}
+ * and {@code timestamp} to the {@code tratt-api}.
+ * <p>
+ * Must be Thread Safe, preferable immutable.
  *
  */
 public interface Event {
+    /**
+     * Returns the identifier of this particular type of event.
+     * <p>
+     * If no specific id exists, then the event name shall be returned.
+     *
+     * @return the identifier of this event.
+     *
+     * @see EventMetaDataFactory
+     */
     String getId();
 
+    /**
+     * Returns the time when this event occurred.
+     *
+     * @return timstamp in milliseconds.
+     */
     long getTimestampMillis();
 }
