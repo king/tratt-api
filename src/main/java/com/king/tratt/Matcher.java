ddf8970df61a72c1adb7ccfcd2cea6025db8e1b2
Alexander Poulikakos <alexander.poulikakos@king.com>
2016-05-23 17:26:39 +0200

    Updated copyright message with License type and project url.

--- src/main/java/com/king/tratt/Matcher.java
+++ src/main/java/com/king/tratt/Matcher.java
@@ -1,7 +1,7 @@
-/*******************************************************************************
- * (C) king.com Ltd 2016
- *
- *******************************************************************************/
+// (C) king.com Ltd 2016
+// https://github.com/king/tratt-api
+// License: Apache 2.0, https://raw.github.com/king/PROJECT/LICENSE-APACHE
+
 package com.king.tratt;
 
 import static com.king.tratt.Tratt.util;
--- src/test/java/com/king/tratt/spi/test/imp/Field.java
+++ src/test/java/com/king/tratt/spi/test/imp/Field.java
@@ -1,7 +1,6 @@
-/*******************************************************************************
- * (C) king.com Ltd 2016
- *
- *******************************************************************************/
+// (C) king.com Ltd 2016
+// https://github.com/king/tratt-api
+// License: Apache 2.0, https://raw.github.com/king/PROJECT/LICENSE-APACHE
 package com.king.tratt.spi.test.imp;
 
 import com.google.gson.JsonObject;
